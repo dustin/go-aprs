1edadd46b6a90be9634616b25f10cd2f326d667d
Dustin Sallings <dustin@spy.net>
2012-01-08 23:27:47 -0800

    Added the starts of my APRS-IS tool.

---
+++ gate/gate.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/textproto"
+	"os"
+
+	"github.com/dustin/aprs.go"
+)
+
+var call, pass, filter, server string
+
+func init() {
+	flag.StringVar(&server, "server", "second.aprs.net:14580", "APRS-IS upstream")
+	flag.StringVar(&call, "call", "", "Your callsign")
+	flag.StringVar(&pass, "pass", "", "Your call pass")
+	flag.StringVar(&filter, "filter", "", "Optional filter for APRS-IS server")
+}
+
+func main() {
+	flag.Parse()
+	if call == "" {
+		fmt.Fprintf(os.Stderr, "Your callsign is required.\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+	if pass == "" {
+		fmt.Fprintf(os.Stderr, "Your call pass is required.\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	conn, err := textproto.Dial("tcp", server)
+	if err != nil {
+		log.Fatalf("Error making contact: %v", err)
+	}
+
+	if filter != "" {
+		filter = fmt.Sprintf(" filter %s", filter)
+	}
+
+	conn.PrintfLine("user %s pass %s vers goaprs 0.1%s", call, pass, filter)
+	for {
+		line, err := conn.ReadLine()
+		if err != nil {
+			log.Fatalf("Error reading line:  %v", err)
+		}
+		if line[0] == '#' {
+			log.Printf("info: %s", line)
+		} else {
+			msg := aprs.ParseAPRSMessage(line)
+			log.Printf("%s said to %s:  ``%s''", msg.Source, msg.Dest, msg.Body)
+		}
+	}
+}
