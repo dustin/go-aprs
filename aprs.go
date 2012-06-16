1763cc5229ca5f15107aae28ce3a51d712a19809
Dustin Sallings <dustin@spy.net>
2012-06-15 22:17:02 -0700

    I'm able to read a few positions now.

--- aprs.go
+++ aprs.go
@@ -2,15 +2,65 @@ package aprs
 
 import (
 	"bytes"
+	"errors"
+	"regexp"
+	"strconv"
 	"strings"
 )
+
+const coordField = `(\d{1,3})([0-5][0-9])\.(\d+)\s*([NEWS])`
+
+var bodyRegexp = regexp.MustCompile(coordField + "/" + coordField)
+
+var NoPositionFound = errors.New("No Positions Found")
+
+type MsgBody string
 
 type APRSMessage struct {
 	Original string
 	Source   string
 	Dest     string
 	Path     []string
-	Body     string
+	Body     MsgBody
+}
+
+// Get the position of the message.
+func (body MsgBody) Position() (lat float64, lon float64, err error) {
+	found := bodyRegexp.FindAllStringSubmatch(string(body), 8)
+	// {"3722.1 N/12159.1 W", "37", "22", "1", "N", "121", "59", "1", "W"}
+	if len(found) == 0 || len(found[0]) != 9 {
+		return 0, 0, NoPositionFound
+	}
+	nums := []float64{0, 0, 0, 0}
+	toparse := []string{found[0][1], found[0][2] + "." + found[0][3],
+		found[0][5], found[0][6] + "." + found[0][7]}
+	for i, p := range toparse {
+		n, err := strconv.ParseFloat(p, 64)
+		if err != nil {
+			return 0, 0, err
+		}
+		nums[i] = n
+	}
+
+	a := nums[0] + (nums[1] / 60)
+	b := nums[2] + (nums[3] / 60)
+
+	if found[0][4] == "S" || found[0][4] == "W" {
+		a = 0 - a
+	}
+	if found[0][8] == "W" || found[0][8] == "S" {
+		b = 0 - b
+	}
+
+	if found[0][4] == "N" || found[0][4] == "S" {
+		lat = a
+		lon = b
+	} else {
+		lat = b
+		lon = a
+	}
+
+	return
 }
 
 func ParseAPRSMessage(i string) APRSMessage {
@@ -22,7 +72,7 @@ func ParseAPRSMessage(i string) APRSMessage {
 	return APRSMessage{Original: i,
 		Source: srcparts[0],
 		Dest:   pathparts[0], Path: pathparts[1:],
-		Body: parts[1]}
+		Body: MsgBody(parts[1])}
 }
 
 func (m *APRSMessage) String() string {
@@ -34,6 +84,6 @@ func (m *APRSMessage) String() string {
 		b.WriteString(strings.Join(m.Path, ","))
 	}
 	b.WriteByte(':')
-	b.WriteString(m.Body)
+	b.WriteString(string(m.Body))
 	return b.String()
 }
--- aprs_test.go
+++ aprs_test.go
@@ -2,6 +2,7 @@ package aprs
 
 import (
 	"encoding/json"
+	"math"
 	"os"
 	"testing"
 )
@@ -19,6 +20,13 @@ func assert(t *testing.T, name string, got interface{}, expected interface{}) {
 	// t.Logf("Looks like %s was %s", name, expected)
 }
 
+func assertEpsilon(t *testing.T, field string, expected, got float64) {
+	if math.Abs(got-expected) > 0.001 {
+		t.Fatalf("Expected %v for %v, got %v -- of by %v",
+			expected, field, got, math.Abs(got-expected))
+	}
+}
+
 func TestAPRS(t *testing.T) {
 	v := ParseAPRSMessage(CHRISTMAS_MSG)
 	assert(t, "Source", v.Source, "KG6HWF")
@@ -26,7 +34,15 @@ func TestAPRS(t *testing.T) {
 	assert(t, "len(Path)", len(v.Path), 2)
 	assert(t, "Path[0]", v.Path[0], "WIDE1-1")
 	assert(t, "Path[1]", v.Path[1], "WIDE2-1")
-	assert(t, "Body", v.Body, "=3722.1 N/12159.1 W-Merry Christmas!")
+	assert(t, "Body", string(v.Body), "=3722.1 N/12159.1 W-Merry Christmas!")
+
+	lat, lon, err := v.Body.Position()
+	if err != nil {
+		t.Fatalf("Couldn't parse body position:  %v", err)
+	}
+
+	assertEpsilon(t, "lat", 37.368333333333, lat)
+	assertEpsilon(t, "lon", -121.985, lon)
 
 	assert(t, "String()", v.String(), CHRISTMAS_MSG)
 }
@@ -37,14 +53,30 @@ type SampleDoc struct {
 	Failed int                    `json:"failed"`
 }
 
+func assertLatLon(t *testing.T, lat, lon float64, doc SampleDoc) {
+	slat, haslat := doc.Result["latitude"].(float64)
+	slon, haslon := doc.Result["longitude"].(float64)
+	if !(haslat && haslon) {
+		return
+	}
+	if math.Abs(lat-slat) > 0.001 || math.Abs(lon-slon) > 0.001 {
+		t.Fatalf("Error parsing lat/lon from %v, got %v,%v; expected %v,%v",
+			doc.Src, lat, lon, slat, slon)
+	}
+
+}
+
 func TestFAP(t *testing.T) {
 	var samples []SampleDoc
-	r, oerr := os.Open("sample.json")
-	if oerr != nil {
+	r, err := os.Open("sample.json")
+	if err != nil {
 		t.Fatalf("Error opening sample.json")
 	}
-	jd := json.NewDecoder(r)
-	jd.Decode(&samples)
+	defer r.Close()
+	err = json.NewDecoder(r).Decode(&samples)
+	if err != nil {
+		t.Fatalf("Error reading JSON: %", err)
+	}
 	t.Logf("Found %d messages", len(samples))
 
 	for _, sample := range samples {
@@ -52,7 +84,13 @@ func TestFAP(t *testing.T) {
 			v := ParseAPRSMessage(sample.Src)
 			assert(t, "Source", v.Source, sample.Result["srccallsign"])
 			assert(t, "Dest", v.Dest, sample.Result["dstcallsign"])
-			assert(t, "Body", v.Body, sample.Result["body"])
+			assert(t, "Body", string(v.Body), sample.Result["body"])
+
+			lat, lon, err := v.Body.Position()
+			if err == nil {
+				assertLatLon(t, lat, lon, sample)
+			}
+
 		}
 	}
 }
