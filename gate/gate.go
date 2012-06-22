979cc91b04aa748d924b22fc62e281d5517f7212
Dustin Sallings <dustin@spy.net>
2012-06-22 01:03:08 -0700

    I saw some of these packets go out, but never got them on my phone.

--- gate/gate.go
+++ gate/gate.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"io"
@@ -104,39 +105,56 @@ func readSerial(ch chan<- aprs.APRSMessage) {
 	}
 }
 
-func sendMessage(w http.ResponseWriter, r *http.Request) {
+func sendMessage(rw http.ResponseWriter, r *http.Request) {
 	src := r.FormValue("src")
 	dest := r.FormValue("dest")
 	text := r.FormValue("msg")
 	if radio == nil {
-		fmt.Fprintf(w, "No radio")
+		fmt.Fprintf(rw, "No radio")
 		return
 	}
 
 	if text != "" {
-		_, err := radio.Write([]byte{0xc0, 0x14, 0x32, 0x43, 0x00})
+		d := hex.Dumper(os.Stdout)
+		defer d.Close()
+		w := io.MultiWriter(d, radio)
+
+		n, err := w.Write([]byte{0xc0, 0x00})
 		if err != nil {
 			log.Fatal(err)
+		}
+		if n != 2 {
+			log.Fatalf("Expected to write two bytes, wrote %v", n)
 		}
 
 		msg := aprs.APRSMessage{
 			Source: aprs.AddressFromString(src),
 			Dest:   aprs.AddressFromString(dest),
-			Body:   aprs.MsgBody(text),
+			Path: []aprs.Address{
+				aprs.AddressFromString("WIDE2-2")},
+			Body: aprs.MsgBody(text),
 		}
 
-		_, err = radio.Write(msg.ToAX25Command())
+		body := msg.ToAX25Command()
+		n, err = w.Write(body)
 		if err != nil {
 			log.Fatal(err)
 		}
-		_, err = radio.Write([]byte{0xc0})
+		if n != len(body) {
+			log.Fatalf("Expected to write %v bytes, wrote %v", len(body), n)
+		}
+
+		n, err = w.Write([]byte{0xc0})
 		if err != nil {
 			log.Fatal(err)
 		}
+		if n != 1 {
+			log.Fatalf("Expected to write 1 byte, wrote %v", n)
+		}
 
-		fmt.Fprintf(w, "Message sent")
+		fmt.Fprintf(rw, "Message sent")
 	} else {
-		fmt.Fprintf(w, "No message")
+		fmt.Fprintf(rw, "No message")
 	}
 }
 
