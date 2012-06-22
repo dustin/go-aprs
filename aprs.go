25005988487b657c19f6f6d89385172c56dd2176
Dustin Sallings <dustin@spy.net>
2012-06-21 18:59:10 -0700

    Understanding ax.25 frames from a KISS source.

--- aprs.go
+++ aprs.go
@@ -2,21 +2,58 @@ package aprs
 
 import (
 	"bytes"
+	"fmt"
+	"strconv"
 	"strings"
 )
 
 type MsgBody string
 
+type Address struct {
+	Call string
+	SSID int
+}
+
+func (a Address) String() string {
+	rv := a.Call
+	if a.SSID != 0 {
+		rv = fmt.Sprintf("%s-%d", a.Call, a.SSID)
+	}
+	return rv
+}
+
 type APRSMessage struct {
 	Original string
-	Source   string
-	Dest     string
-	Path     []string
+	Source   Address
+	Dest     Address
+	Path     []Address
 	Body     MsgBody
 }
 
 func (b MsgBody) Type() PacketType {
 	return PacketType(b[0])
+}
+
+func parseAddress(s string) Address {
+	parts := strings.Split(s, "-")
+	rv := Address{Call: parts[0]}
+	if len(parts) > 1 {
+		x, err := strconv.ParseInt(parts[1], 10, 32)
+		if err == nil {
+			rv.SSID = int(x)
+		}
+	}
+	return rv
+}
+
+func parseAddresses(addrs []string) []Address {
+	rv := []Address{}
+
+	for _, s := range addrs {
+		rv = append(rv, parseAddress(s))
+	}
+
+	return rv
 }
 
 func ParseAPRSMessage(i string) APRSMessage {
@@ -26,18 +63,19 @@ func ParseAPRSMessage(i string) APRSMessage {
 	pathparts := strings.Split(srcparts[1], ",")
 
 	return APRSMessage{Original: i,
-		Source: srcparts[0],
-		Dest:   pathparts[0], Path: pathparts[1:],
-		Body: MsgBody(parts[1])}
+		Source: parseAddress(srcparts[0]),
+		Dest:   parseAddress(pathparts[0]),
+		Path:   parseAddresses(pathparts[1:]),
+		Body:   MsgBody(parts[1])}
 }
 
 func (m *APRSMessage) String() string {
-	b := bytes.NewBufferString(m.Source)
+	b := bytes.NewBufferString(m.Source.String())
 	b.WriteByte('>')
-	b.WriteString(m.Dest)
-	if len(m.Path) > 0 {
+	b.WriteString(m.Dest.String())
+	for _, p := range m.Path {
 		b.WriteByte(',')
-		b.WriteString(strings.Join(m.Path, ","))
+		b.WriteString(p.String())
 	}
 	b.WriteByte(':')
 	b.WriteString(string(m.Body))
---
+++ ax25/frames.go
@@ -0,0 +1,79 @@
+package ax25
+
+import (
+	"bufio"
+	"errors"
+	"io"
+	"strings"
+
+	"github.com/dustin/aprs.go"
+)
+
+const reasonableSize = 14
+
+var shortMessage = errors.New("short message")
+var truncatedMessage = errors.New("truncated message")
+
+func parseAddr(in []byte) aprs.Address {
+	out := make([]byte, len(in))
+	for i, b := range in {
+		out[i] = b >> 1
+	}
+	rv := aprs.Address{
+		Call: strings.TrimSpace(string(out[:len(out)-1])),
+		SSID: int((out[len(out)-1]) & 0xf),
+	}
+	return rv
+}
+
+func decodeMessage(frame []byte) (rv aprs.APRSMessage, err error) {
+	frame = frame[:len(frame)-1]
+
+	if len(frame) < reasonableSize {
+		err = shortMessage
+		return
+	}
+
+	rv.Source = parseAddr(frame[8:15])
+	rv.Dest = parseAddr(frame[1:8])
+
+	rv.Path = []aprs.Address{}
+
+	frame = frame[15:]
+	for len(frame) > 7 && frame[0] != 3 {
+		rv.Path = append(rv.Path, parseAddr(frame[:7]))
+		frame = frame[7:]
+	}
+
+	if len(frame) < 2 || frame[0] != 3 || frame[1] != 0xf0 {
+		err = truncatedMessage
+		return
+	}
+
+	rv.Body = aprs.MsgBody(string(frame[2:]))
+
+	return
+}
+
+// An AX.25 message decoder.
+type Decoder struct {
+	r *bufio.Reader
+}
+
+// Get the next message.
+func (d *Decoder) Next() (aprs.APRSMessage, error) {
+	frame := []byte{}
+	var err error
+	for len(frame) < reasonableSize {
+		frame, err = d.r.ReadBytes(byte(0xc0))
+		if err != nil {
+			return aprs.APRSMessage{}, err
+		}
+	}
+	return decodeMessage(frame)
+}
+
+// Get a new decoder over this reader.
+func NewDecoder(r io.Reader) *Decoder {
+	return &Decoder{bufio.NewReader(r)}
+}
