e423d17a821a058ab7c41be82eec6d7a59ef3d1c
Dustin Sallings <dustin@spy.net>
2013-07-30 09:35:40 -0700

    Fix priority level conversion for nma

--- gate/notifications.go
+++ gate/notifications.go
@@ -53,7 +53,7 @@ func notifyMyAndroid(n notifier, note notification) (err error) {
 		Application: n.Config["application"],
 		Description: note.Msg,
 		Event:       note.Event,
-		Priority:    i,
+		Priority:    nma.PriorityLevel(i),
 	}
 
 	return notifier.Notify(&msg)
